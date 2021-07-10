1655743ceac84da9495e334854f52ae77797de5b
jaewan <t1653tt@gmail.com>
2021-07-10 16:36:05 +0900

    20210710_01

--- src/main/java/com/project/one/controller/MemberController.java
+++ src/main/java/com/project/one/controller/MemberController.java
@@ -283,5 +283,15 @@ public class MemberController {
 		
 		return "main";
 	}
+	
+	@RequestMapping("/mypage.do")
+	public String mypage(Model model, String member_id) {
+		MemberDto dto = biz.selectOne(member_id);
+		
+		model.addAttribute("dto",dto);
+		
+		return "mypage";
+		
+	}
 
 }
