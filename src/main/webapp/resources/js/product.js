32dbc6ce489694eb33e0d5a6f504b9a4f6957545
Soojung Kwak <gsj2464@gmail.com>
2021-07-30 20:02:32 +0900

    auto completed

--- src/main/webapp/resources/js/product.js
+++ src/main/webapp/resources/js/product.js
@@ -1,6 +1,4 @@
 $(function() {
-	on_off();
-	
 	let autoChecked = $("#member_auto").val();
 
 	let category = "${category}";
@@ -22,57 +20,51 @@ $(function() {
 
 	if (autoChecked === 'Y') {
 		$("#default-switch").attr('checked', true);
+		getAutoCompleted();
 	}
 
 	let onoff = $("[name=onoff]");
 	onoff.click(function() {
 		$("[name=on_off]").toggle();
-
-		on_off();
+		getAutoCompleted();
 	});
 
 	$("#close").click(function() {
 		$("#auto_result").empty();
+		$("[name=search_keyword]:eq(0)").val("");
 	});
 
 });
 
-function on_off() {
-	if ($("#on").is(":visible")) {
-		alert("on")
-		getAutoCompleted();
-		
-	} else {
-		alert("off")
-		$("#auto_result").empty();
-	}
-}
-
 function getAutoCompleted() {
 	$("[name=search_keyword]:eq(0)").change(function() {
-		$.ajax({
-			type: "post",
-			url: "auto_product.do",
-			data: JSON.stringify({ "keyword": $("[name=search_keyword]:eq(0)").val() }),
-			contentType: "application/json",
-			dataType: "json",
-			success: function(result) {
-				let list = result.list;
-
-				for (let i = 0; i < list.length; i++) {
-					let input = $(`<input id=${i} type='text' class='form-control bg-white border-right' style="cursor:pointer;" value= ${list[i]} readonly />`);
-					$("#auto_result").append(input);
-
-					$(`#${i}`).click(function() {
-						let newInput = $(`#${i}`).val();
-						$("[name=search_keyword]:eq(0)").val(newInput);
-					});
+		if ($("#on").is(":visible")) {
+			$.ajax({
+				type: "post",
+				url: "auto_product.do",
+				data: JSON.stringify({ "keyword": $("[name=search_keyword]:eq(0)").val() }),
+				contentType: "application/json",
+				dataType: "json",
+				success: function(result) {
+					let list = result.list;
+	
+					for (let i = 0; i < list.length; i++) {
+						let input = $(`<input id=${i} type='text' class='form-control bg-white border-right' style="cursor:pointer;" value= ${list[i]} readonly />`);
+						$("#auto_result").append(input);
+	
+						$(`#${i}`).click(function() {
+							let newInput = $(`#${i}`).val();
+							$("[name=search_keyword]:eq(0)").val(newInput);
+						});
+					}
+	
+				},
+				error: function() {
+					alert("통신 실패!");
 				}
-
-			},
-			error: function() {
-				alert("통신 실패!");
-			}
-		});
+			});
+		} else if(!$("#on").is(":visible")){
+			$("#auto_result").empty();
+		}
 	});
 }
